e23a31bd88637cb8ec6074a68c7ffacb65ebdd0a
Connor Slade <connor@connorcode.com>
2024-02-07 22:01:48 -0500

    Sequential digits problem

--- sequential_digits/problem.md
+++ sequential_digits/problem.md
@@ -1,5 +1,7 @@
 # Sequential Digits
 
+- [Problem on Leetcode](https://leetcode.com/problems/sequential-digits)
+
 An integer has sequential digits if and only if each digit in the number is one more than the previous digit.
 Return a sorted list of all the integers in the range [low, high] inclusive that have sequential digits.
 
--- sequential_digits/sequential_digits.cpp
+++ sequential_digits/sequential_digits.cpp
@@ -1,9 +1,25 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 std::vector<int> sequential_digits(const uint32_t low, const uint32_t high)
 {
-    return {};
+    auto out = std::vector<int>{};
+    for (uint32_t start = 1; start <= 9; start++)
+    {
+        uint32_t num = start;
+        for (uint32_t next = start + 1; next <= 9; next++)
+        {
+            num = num * 10 + next;
+            if (num > high)
+                break;
+            if (num >= low)
+                out.push_back(num);
+        }
+    }
+
+    std::sort(out.begin(), out.end());
+    return out;
 }
 
 int main()
