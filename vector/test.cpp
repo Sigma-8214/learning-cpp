d4e0e2ec181c520e875f4d535c05d191a711cd03
Connor Slade <connor@connorcode.com>
2024-02-10 15:38:08 -0500

    Vector impl

---
+++ vector/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "vector.hpp"
+
+using namespace vector;
+
+int main()
+{
+start:
+    auto vec = Vector<3>::from({1.0, 10.0, 5.0});
+    auto vec2 = Vector<3>::from({2.0, 20.0, 5.0});
+
+    std::cout << vec.to_string() << std::endl;
+    std::cout << vec2.to_string() << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Add: " << (vec + vec2).to_string() << std::endl;
+    std::cout << "Sub: " << (vec - vec2).to_string() << std::endl;
+    std::cout << "Cross: " << vec.cross(vec2).to_string() << std::endl;
+
+    return 0;
+}
---
+++ vector/vector.cpp
@@ -0,0 +1,13 @@
+#include "vector.hpp"
+
+namespace vector
+{
+    // template <size_t N>
+    // Vector<N>::Vector()
+    // {
+    //     for (size_t i = 0; i < N; i++)
+    //     {
+    //         data[i] = 0;
+    //     }
+    // }
+}
