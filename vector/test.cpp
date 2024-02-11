7d8b757f8503a2bf94fffdcc818025b3360b4f2f
Connor Slade <connor@connorcode.com>
2024-02-10 20:34:37 -0500

    Update vector impl

--- vector/test.cpp
+++ vector/test.cpp
@@ -8,13 +8,14 @@ int main() {
     auto vec = Vector<3>::from({1.0, 10.0, 5.0});
     auto vec2 = Vector<3>::from({2.0, 20.0, 5.0});
 
-    std::cout << vec.to_string() << std::endl;
-    std::cout << vec2.to_string() << std::endl;
+    std::cout << vec << std::endl;
+    std::cout << vec2 << std::endl;
     std::cout << std::endl;
 
-    std::cout << "Add: " << (vec + vec2).to_string() << std::endl;
-    std::cout << "Sub: " << (vec - vec2).to_string() << std::endl;
-    std::cout << "Cross: " << vec.cross(vec2).to_string() << std::endl;
+    std::cout << "Add: " << vec + vec2 << std::endl;
+    std::cout << "Sub: " << vec - vec2 << std::endl;
+    std::cout << "Scaler: " << 3.0 * vec << std::endl;
+    std::cout << "Cross: " << vec.cross(vec2) << std::endl;
 
     return 0;
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -8,6 +8,7 @@ template <size_t N> class Vector {
   public:
     std::array<Num, N> data;
 
+    /// @brief Creates a new vector at the origin.
     static Vector empty() {
         Vector result;
         for (size_t i = 0; i < N; i++)
@@ -15,6 +16,7 @@ template <size_t N> class Vector {
         return result;
     }
 
+    /// @brief Creates a new vector from an array.
     static Vector from(std::array<Num, N> data) {
         Vector result;
         for (size_t i = 0; i < N; i++)
@@ -22,15 +24,82 @@ template <size_t N> class Vector {
         return result;
     }
 
-    Vector<N> cross(Vector<N> other) {
-        Vector<N> result;
+    /// @brief  Makes a copy of a vector.
+    Vector clone() const {
+        Vector result;
+        for (size_t i = 0; i < N; i++)
+            result.data[i] = data[i];
+        return result;
+    }
+
+    /// @brief Computes the cross product of two vectors.
+    Vector cross(Vector<N> other) const {
+        Vector result;
         for (size_t i = 0; i < N; i++)
             result.data[i] = data[(i + 1) % N] * other.data[(i + 2) % N] -
                              data[(i + 2) % N] * other.data[(i + 1) % N];
         return result;
     }
 
-    std::string to_string() {
+    /// @brief Computes the hadamard product of two vectors. (component-wise
+    /// multiplication)
+    Vector hadamard_product(Vector<N> other) const {
+        Vector result;
+        for (size_t i = 0; i < N; i++)
+            result.data[i] = data[i] * other.data[i];
+        return result;
+    }
+
+    /// @brief Computes the dot product of two vectors.
+    Num dot(Vector<N> other) const {
+        auto acc = 0.0;
+        for (size_t i = 0; i < N; i++)
+            acc += data[i] * other.data[i];
+        return acc;
+    }
+
+    /// @brief Adds two vectors.
+    Vector operator+(const Vector &other) const {
+        Vector result;
+        for (size_t i = 0; i < N; i++)
+            result.data[i] = data[i] + other.data[i];
+        return result;
+    }
+
+    /// @brief Subtracts two vectors
+    Vector operator-(const Vector &other) const {
+        Vector result;
+        for (size_t i = 0; i < N; i++)
+            result.data[i] = data[i] - other.data[i];
+        return result;
+    }
+
+    /// @brief Negates a vector. (Inverts the sign of all components)
+    Vector operator-() const {
+        Vector result;
+        for (size_t i = 0; i < N; i++)
+            result.data[i] = -data[i];
+        return result;
+    }
+
+    /// @brief Scales a vector by a scalar. (Multiplies all components by a
+    /// scalar)
+    Vector operator*(const Num &scalar) const {
+        Vector result;
+        for (size_t i = 0; i < N; i++)
+            result.data[i] = data[i] * scalar;
+        return result;
+    }
+
+    /// @brief Scales a vector by a scalar. (Multiplies all components by a
+    /// scalar)
+    friend Vector operator*(const Num &scalar, const Vector &vec) {
+        return vec * scalar;
+    }
+
+    /// @brief Generates a string representation of a vector in the format: `[a,
+    /// b, c ...]`.
+    std::string to_string() const {
         std::string result = "[";
         for (size_t i = 0; i < N; i++) {
             result += std::to_string(data[i]);
@@ -41,18 +110,11 @@ template <size_t N> class Vector {
         return result;
     }
 
-    Vector operator+(const Vector &other) {
-        Vector result;
-        for (size_t i = 0; i < N; i++)
-            result.data[i] = data[i] + other.data[i];
-        return result;
-    }
-
-    Vector operator-(const Vector &other) {
-        Vector result;
-        for (size_t i = 0; i < N; i++)
-            result.data[i] = data[i] - other.data[i];
-        return result;
+    /// @brief Overloads the `<<` operator to allow for easy printing of
+    /// vectors.
+    friend std::ostream &operator<<(std::ostream &os, const Vector &vec) {
+        os << vec.to_string();
+        return os;
     }
 };
-}
+} // namespace vector
