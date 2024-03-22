a48e844ff6d919d85c0701461dbdf27c834ccc7a
Connor Slade <connor@connorcode.com>
2024-03-21 20:12:48 -0400

    Move word shower to a new repo

--- word_shower/consts.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "lab_color.hpp"
-
-const float32_t PI = 3.14159265358979;
-const OkLab COLOR_START = OkLab::create(.773, 0.1131, .0);
-const uint32_t HIGH_SCORES_COUNT = 5;
--- word_shower/drawing.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "../tui_engine/tui.hpp"
-
-void draw_rainbow_text(Point2i pos, std::string text, float32_t *hue, Gui &gui);
-Color rainbow_color(float32_t hue, float32_t percent);
